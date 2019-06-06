5c0bd080098337c21d90bc96b1e40b4a662dc7c8
Havok <contact@aussitot.net>
2019-06-06 17:13:45 +0200

    Class construction

--- d2lExemple.php
+++ d2lExemple.php
@@ -1,8 +1,30 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 require("config.php");
 require("phpD2lAPI.php"); //php class API for D2l
 
-$D2l = new D2l($loginD2l, $passwordD2l);
-$Ids = $D2l->getD2lIds();
-print_r($Ids);
- ?>
+$D2l = new D2l($loginD2lReel, $passwordD2lReel);
+if ($D2l->error)
+{
+  echo $D2l->error;
+} else {
+
+  $LastIndexes = $D2l->getLastIndexes();
+  if ($D2l->error)
+  {
+    echo $D2l->error;
+  } else {
+    print_r($LastIndexes);
+  }
+
+  $IndexesBetween = $D2l->getIndexesBetween('2019-06-06T12:00:00','2019-06-06T12:10:00');
+  if ($D2l->error)
+  {
+    echo $D2l->error;
+  } else {
+    print_r($IndexesBetween);
+  }
+}
+?>
